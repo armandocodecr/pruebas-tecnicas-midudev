610d31203ab2e5b78a62b6b4000b07cb41f6e7b3
ArmandoCode <totovip1464@gmail.com>
2023-08-28 12:07:37 -0600

    add footer component to main.tsx

--- 01-books-list/src/main.tsx
+++ 01-books-list/src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import "./Responsive.css";
+import { Footer } from './components/';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>,
 )
