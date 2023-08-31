4afe7f59909904dac2b13ba76e4f7b15ec9c1a9e
ArmandoCode <totovip1464@gmail.com>
2023-08-31 11:54:20 -0600

    fix tests

--- 01-books-list/src/components/__test__/LikeComponent.test.tsx
+++ 01-books-list/src/components/__test__/LikeComponent.test.tsx
@@ -1,4 +1,4 @@
-import { test, describe } from 'vitest'
+import { test, describe, vi, expect } from 'vitest'
 import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 import { LikeComponent } from '../LikeComponent';
 import '@testing-library/jest-dom'
@@ -6,25 +6,26 @@ import '@testing-library/jest-dom'
 describe('Test to LikeComponent', () => {
     afterEach(cleanup)
     test('should render LikeComponent', () => {
-        let isLiked = false
-        const onChangeLiked = () =>  isLiked = !isLiked
-        render(<LikeComponent isChecked onClickFunction={onChangeLiked} />)
+        const onClickMock = vi.fn()
+        render(<LikeComponent isChecked onClickFunction={onClickMock} />)
         const inputSearchElement = screen.getByTestId('like-checkbox');
         expect(inputSearchElement).toBeInTheDocument();
     })
 
     test('should change the state of the input when executing onChange', () => {
-
-        let isLiked = false
-        const onChangeLiked = () =>  isLiked = !isLiked
-        const { getByTestId  } = render(<LikeComponent isChecked={isLiked} onClickFunction={onChangeLiked} />)
+        const onClickMock =  vi.fn()
+        const { getByTestId  } = render(<LikeComponent isChecked onClickFunction={onClickMock} />)
         const inputSearchElement = getByTestId('like-checkbox') as HTMLInputElement;
+
+        fireEvent.click(inputSearchElement)
+        expect(onClickMock).toHaveBeenCalledTimes(1)
+        fireEvent.click(inputSearchElement)
+        expect(onClickMock).toHaveBeenCalledTimes(2)
+
         fireEvent.change(inputSearchElement, { target: { checked: true } }) 
-        fireEvent.change(inputSearchElement, onChangeLiked()) 
-
-        expect(isLiked).toBe(true)
         expect(inputSearchElement.checked).toBe(true)
-
+        fireEvent.change(inputSearchElement, { target: { checked: false } }) 
+        expect(inputSearchElement.checked).toBe(false)
     })
     
 })
